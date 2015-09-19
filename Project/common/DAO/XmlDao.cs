6e5d3fefeda0a21632d2b97beeb96234063d37b0
hddevteam <mengmengxiao@msn.com>
2015-09-19 11:41:09 +0800

    add new revision

---
+++ Project/common/DAO/XmlDao.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+using System.Xml.Linq;
+using common.DTO;
+
+namespace common.DAO
+{
+    public class XmlDao
+    {
+
+        private XmlDocument xmlDoc = new XmlDocument();
+        private XElement xdoc;
+
+        /// <summary>
+        /// 添加项目
+        /// </summary
+        /// <param name="nodes">增加节点集</param>
+        /// <param name="property">节点的属性集</param>
+        /// <param name="xmlConfigPath">添加节点的xml文件</param>
+        /// <returns></returns>
+        public XElement AddXElement(Dictionary<string,string> nodes , Dictionary<string,string> property , string xmlConfigPath)
+        {
+            xdoc = XElement.Load(xmlConfigPath);
+            try
+            {
+                foreach (var item in nodes)
+                {
+                    XElement newElement = new XElement(item.Key);
+                    foreach (var attribute in property)
+                    {
+                        newElement.SetAttributeValue(attribute.Key,attribute.Value);
+                    }
+                    xdoc.Add(newElement);
+                }
+                return xdoc.Elements().Last();
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return xdoc;
+            }
+        }
+
+        /// <summary>
+        /// 查询单个节点数据
+        /// </summary>
+        /// <param name="nodePath">要查询数据的节点</param>
+        /// <param name="num">要查询数据的索引号</param>
+        /// <returns>返回数据字符串</returns>
+        public string XmlRead(string nodePath, int num, string xmlConfigPath)
+        {
+            try
+            {
+                xmlDoc.Load(xmlConfigPath);
+                XmlNodeList nodeList = xmlDoc.SelectSingleNode(nodePath).ChildNodes;
+                return nodeList[num].InnerText;
+            }
+            catch (Exception exception)
+            {
+                return exception.Message;
+            }
+        }
+
+        /// <summary>
+        /// 对节点的子节点进行修改
+        /// </summary>
+        /// <param name="value">需要修改的子节点的键值对</param>
+        /// <param name="property">需要筛选的属性值的键值对</param>
+        /// <param name="xmlPath">xml文件的路径</param>
+        /// <param name="nodePath">节点的路径</param>
+        /// <returns></returns>
+        public string ModifyXNode(Dictionary<string, string> value, XElement xElement, string xmlPath)
+        {
+            try
+            {
+                foreach (var item in value)
+                {
+                    xElement.Element(item.Key).SetValue((item.Value));
+                }
+                xdoc.Save(xmlPath);
+                return "successful";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return "failed";
+            }
+        }
+
+        /// <summary>
+        /// 增加节点
+        /// </summary>
+        /// <param name="value">需要修改的子节点的键值对</param>
+        /// <param name="property">需要筛选的属性值的键值对</param>
+        /// <param name="xmlPath">xml文件的路径</param>
+        /// <param name="nodePath">节点的路径</param>
+        /// <returns></returns>
+        public string AddXNode(Dictionary<string, string> value, XElement xElement, string xmlPath)
+        {
+            try
+            {
+                if (value != null)
+                {
+                    foreach (var item in value)
+                    {
+                        XElement newXElement = new XElement(item.Key, item.Value);
+                        xElement.Add(newXElement);
+                    }
+                }
+                xdoc.Save(xmlPath);
+                return "successful";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return "failed";
+            }
+        }
+
+        /// <summary>
+        /// 筛选所需要的唯一节点元素
+        /// </summary>
+        /// <param name="property">需要满足的属性</param>
+        /// <param name="xmlPath">读取的xml文件路径</param>
+        /// <param name="nodePath">节点路径</param>
+        /// <returns></returns>
+        public XElement SelectOneXElement(Dictionary<string, string> property, string xmlPath,
+            string nodePath)
+        {
+            //若键值对不为0,则需要对属性值进行筛选
+            xdoc = XElement.Load(xmlPath);
+            if (property != null)
+            {
+                IEnumerable<XElement> xElements = xdoc.Elements(nodePath);
+                xElements = property.Aggregate(xElements,
+                    (current, item) =>
+                        (from e in current where e.Attributes(item.Key).First().Value.Equals(item.Value) select e));
+                XElement xElement = xElements.First();
+                return xElement;
+            }
+            else
+            {
+                XElement xElement = xdoc.Element(nodePath);
+                return xElement;
+            }
+        }
+
+        /// <summary>
+        /// 修改节点元素的属性
+        /// </summary>
+        /// <param name="property">修改属性的键值对</param>
+        /// <param name="xElement">所要修改的节点元素</param>
+        /// <param name="xmlPath">保存的xml文件路径</param>
+        /// <returns></returns>
+        public string XNodeAttributes(Dictionary<string, string> property,
+            XElement xElement, string xmlPath)
+        {
+            try
+            {
+                foreach (var item in property)
+                {
+                    xElement.SetAttributeValue(item.Key, item.Value);
+                }
+                xdoc.Save(xmlPath);
+                return "successful";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return "failed";
+            }
+        }
+
+        /// <summary>
+        /// 节点删除操作
+        /// </summary>
+        /// <param name="nodePath">要删除的节点</param>
+        /// <param name="num">节点的索引</param>
+        /// <param name="xmlConfigPath">所需删除节点的一系列xml文件</param>
+        public string XElementDelete(string nodePath, string nodeName, string[] xmlConfigPath)
+        {
+            string result = "";
+            foreach (string t in xmlConfigPath)
+            {
+                xmlDoc.Load(t);
+                try
+                {
+                    var xmlNode = xmlDoc.SelectNodes(nodePath);
+                    for (int i = 0; i < xmlNode.Count; i++)
+                    {
+                        result = "failed";
+                        if (xmlNode[i].Attributes["Name"].InnerText == nodeName)
+                        {
+                            xmlNode[i].ParentNode.RemoveChild(xmlNode[i]);
+                            xmlDoc.Save(t);
+                            result = "successful";
+                            break;
+                        }
+                    }                 
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                    result = "failed";
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 获取节点
+        /// </summary>
+        /// <param name="nodePath">节点路径</param>
+        /// <param name="xmlPath">读取的xml文件的路径</param>
+        /// <returns>返回节点list</returns>
+        public XmlNodeList XmlQuery(string nodePath, string xmlPath)
+        {
+            xmlDoc.Load(xmlPath);
+            XmlNodeList xmlNodeList = xmlDoc.SelectNodes(nodePath);
+            return xmlNodeList;
+        }
+    }
+}
---
+++ Project/common/DTO/ProjectInfo.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.DTO
+{
+   public class ProjectInfo
+    {
+        //------------------ProjectInfo类封装的私有属性---------------------------------------
+        //project的name属性值
+        private string nameProperty;
+        //project的status属性值
+        private string statusProperty;
+        //project的svn服务器路径
+        private string repositoryPath;
+        //project的本地工作路径
+        private string workDirectory;
+        //project的编译语句
+        private string buildCommand;
+        //project的编译后返回的日志
+        private string log;
+        //邮件接收者
+        private string mailTo;
+       //邮件域名
+        private string mailHost;
+       //发件人
+       private string userName;
+       //密码
+       private string password;
+        //project的编译结果
+        private string result;
+        //project的当前版本上传人
+        private string author;
+        //project的当前版本号
+        private string revision;
+        //project的当前版本修改时间
+        private string changeTime;
+        //project的编译时间
+        private string duration;
+        //lastProject的编译时间
+        private string buildTime;
+       //是否在slack中传输信息
+        private string ifSlack;
+        //是否发送邮件
+        private string ifMail;
+        //slack的网址
+        private string slackUrl;
+        //slack的频道
+        private string slackChannel;
+        //slack的发送者(default)
+        private string slackUser;
+        //slack的发送内容
+        private string slackContent;
+        //project index
+        private string index;
+        
+        //---------------------------------------------------------
+
+        //------------------ProjectInfo类的无参数构造方法---------------------------------------
+        public ProjectInfo()
+        {
+            this.nameProperty = "";
+            this.statusProperty = "";
+            this.repositoryPath = "";
+            this.workDirectory = "";
+            this.buildCommand = "";
+            this.log = "";
+            this.result = "";
+            this.author = "";
+            this.revision = "";
+            this.changeTime = "";
+            this.duration = "";
+            this.mailTo = "";
+            this.IfMail = "";
+            this.IfSlack = "";
+            this.SlackUrl = "";
+            this.SlackChannel = "";
+        }
+        //---------------------------------------------------------
+
+        //------------------ProjectInfo类对外提供的用于访问私有属性的public方法---------------------------------------
+        public string Nameproperty
+        {
+            get { return nameProperty; }
+            set { nameProperty = value; }
+        }
+
+        public string Statusproperty
+        {
+            get { return statusProperty; }
+            set { statusProperty = value; }
+        }
+
+        public string Repositorypath
+        {
+            get { return repositoryPath; }
+            set { repositoryPath = value; }
+        }
+
+        public string Workdirectory
+        {
+            get { return workDirectory; }
+            set { workDirectory = value; }
+        }
+
+        public string Buildcommand
+        {
+            get { return buildCommand; }
+            set { buildCommand = value; }
+        }
+
+        public string Log
+        {
+            get { return log; }
+            set { log = value; }
+        }
+
+        public string Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public string Revision
+        {
+            get { return revision; }
+            set { revision = value; }
+        }
+
+        public string Changetime
+        {
+            get { return changeTime; }
+            set { changeTime = value; }
+        }
+
+        public string MailTo
+        {
+            get { return mailTo;}
+            set { mailTo = value;}
+        }
+
+        public string Duration
+        {
+            get { return duration; }
+            set { duration = value; }
+        }
+
+        public string BuildTime
+        {
+            get
+            {
+                return buildTime;
+            }
+
+            set
+            {
+                buildTime = value;
+            }
+        }
+
+       public string IfSlack
+       {
+           get { return ifSlack; }
+           set { ifSlack = value; }
+       }
+
+       public string IfMail
+       {
+           get { return ifMail; }
+           set { ifMail = value; }
+       }
+
+       public string SlackUrl
+       {
+           get { return slackUrl; }
+           set { slackUrl = value; }
+       }
+
+       public string SlackChannel
+       {
+           get { return slackChannel; }
+           set { slackChannel = value; }
+       }
+
+       public string MailHost
+       {
+           get { return mailHost; }
+           set { mailHost = value; }
+       }
+
+       public string UserName
+       {
+           get { return userName; }
+           set { userName = value; }
+       }
+
+       public string Password
+       {
+           get { return password; }
+           set { password = value; }
+       }
+
+        public string Index
+        {
+            get
+            {
+                return index;
+            }
+
+            set
+            {
+                index = value;
+            }
+        }
+
+       public string SlackUser
+       {
+           get { return slackUser; }
+           set { slackUser = value; }
+       }
+
+       public string SlackContent
+       {
+           get { return slackContent; }
+           set { slackContent = value; }
+       }
+
+       //---------------------------------------------------------
+    }
+}
